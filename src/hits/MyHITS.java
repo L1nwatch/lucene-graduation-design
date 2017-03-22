fe9b6aaab519d12f25d0798f2a98357f82377852
L1n <490772448@qq.com>
2017-03-22 15:56:08 +0800

    参考 WIKI 实现了另外一个版本的 HITS 算法, 感觉原来的那种 HITS 算法存在逻辑问题, 还是 WIKI 上的权威一些吧, 而且和论文描述的伪码较为接近

--- src/hits/MyHITS.java
+++ src/hits/MyHITS.java
@@ -44,10 +44,12 @@ public class MyHITS {
         return this.pagesList;
     }
 
+
     /**
-     * 输出结果页面，也就是authority权威值最高的页面
+     * 输出结果页面，也就是 authority 权威值最高的页面
+     * 以下代码参考: http://blog.csdn.net/androidlushangderen/article/details/43311943
      */
-    public ArrayList<News> hitsSort(boolean[][] linkMatrix) {
+    public ArrayList<News> hitsSort2(boolean[][] linkMatrix) {
         //误差值，用于收敛判断
         double error = Integer.MAX_VALUE;
 
@@ -65,9 +67,11 @@ public class MyHITS {
             // resultHub 和 resultAuthority 值的更新计算
             for (int i = 0; i < pageNum; i++) {
                 for (int j = 0; j < pageNum; j++) {
+                    // i 指向了 j
                     if (linkMatrix[i][j]) {
-                        // i 指向了 j
+                        // 页面 p 的中心值 = 所有页面 p 指向的页面的权威值之和
                         temp_hub[i] += resultAuthority[j];
+                        // 页面 p 的权威值 = 所有指向页面 p 的页面的中心值之和
                         temp_authority[j] += resultHub[i];
                     }
                 }
@@ -85,8 +89,63 @@ public class MyHITS {
 
                 error += Math.abs(temp_authority[k] - resultHub[k]);
 
-                resultHub[k] = temp_authority[k];
-                resultAuthority[k] = temp_hub[k];
+                resultHub[k] = temp_hub[k];
+                resultAuthority[k] = temp_authority[k];
+            }
+        }
+
+        System.out.println(String.format("[*] 最终收敛成功"));
+
+        return sortByHITSResult(resultAuthority);
+    }
+
+    /**
+     * 输出结果页面，也就是 authority 权威值最高的页面
+     * 以下代码参考 wiki: https://en.wikipedia.org/wiki/HITS_algorithm
+     */
+    public ArrayList<News> hitsSort(boolean[][] linkMatrix) {
+        // 初始化 resultAuthority 和 resultHub, 默认值都为 1
+        double[] resultAuthority = new double[pageNum];//网页Authority权威值
+        double[] resultHub = new double[pageNum];//网页hub中心值
+        Arrays.fill(resultAuthority, 1);
+        Arrays.fill(resultHub, 1);
+
+        // 进行 k 次, 论文中给出的 k 值为 20
+        int p, q, r;
+        for (int k = 0; k < 20; ++k) {
+            double norm = 0.0;
+
+            // 更新权威值
+            for (p = 0; p < pageNum; ++p) {
+                resultAuthority[p] = 0;
+                for (q = 0; q < pageNum; ++q) {
+                    // q 指向 p
+                    if (linkMatrix[q][p]) {
+                        resultAuthority[p] += resultHub[q];
+                    }
+                }
+                norm += Math.pow(resultAuthority[p], 2);
+            }
+            norm = Math.sqrt(norm);
+            for (p = 0; p < pageNum; ++p) {
+                resultAuthority[p] = resultAuthority[p] / norm;
+            }
+
+            // 更新中心值
+            norm = 0.0;
+            Arrays.fill(resultHub, 0);
+            for (p = 0; p < pageNum; ++p) {
+                for (r = 0; r < pageNum; ++r) {
+                    // p 指向 r
+                    if (linkMatrix[p][r]) {
+                        resultHub[p] += resultAuthority[r];
+                    }
+                }
+                norm += Math.pow(resultHub[p], 2);
+            }
+            norm = Math.sqrt(norm);
+            for (p = 0; p < pageNum; ++p) {
+                resultHub[p] = resultHub[p] / norm;
             }
         }
 
